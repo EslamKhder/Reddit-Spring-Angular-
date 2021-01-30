969ad3a23086f36f0b4a936145efc96ff47a4dba
EslamKhder <eslamkhder81@gmail.com>
2021-01-30 21:58:05 +0200

    sign up & Verification email

---
+++ back-end/src/main/java/com/springboot/reddit/exceptions/PostNotFoundException.java
@@ -0,0 +1,6 @@
+package com.springboot.reddit.exceptions;
+
+public class PostNotFoundException extends RuntimeException {
+    public PostNotFoundException(String message) {
+    }
+}
---
+++ back-end/src/main/java/com/springboot/reddit/services/MailService.java
@@ -0,0 +1,45 @@
+package com.springboot.reddit.services;
+import com.springboot.reddit.exceptions.SpringRedditException;
+import com.springboot.reddit.model.NotificationEmail;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+class MailService {
+
+    private final JavaMailSender mailSender;
+    private final MailContentBuilder mailContentBuilder;
+
+    @Autowired
+    public MailService(JavaMailSender mailSender, MailContentBuilder mailContentBuilder) {
+        this.mailSender = mailSender;
+        this.mailContentBuilder = mailContentBuilder;
+    }
+
+    @Async
+    void sendMail(NotificationEmail notificationEmail) {
+        MimeMessagePreparator messagePreparator = mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setFrom("springreddit@email.com");
+            messageHelper.setTo(notificationEmail.getRecipient());
+            messageHelper.setSubject(notificationEmail.getSubject());
+            messageHelper.setText(notificationEmail.getBody());
+        };
+        try {
+            mailSender.send(messagePreparator);
+            log.info("Activation email sent!!");
+        } catch (MailException e) {
+            log.error("Exception occurred when sending mail", e);
+            throw new SpringRedditException("Exception occurred when sending mail to " + notificationEmail.getRecipient(), e);
+        }
+    }
+
+}
